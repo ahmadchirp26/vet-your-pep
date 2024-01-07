2034ed8e51296cea07b0b5c9f052f7a66ec4723d
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-01-08 01:43:36 +0500

    chore: done project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+// import "../styles/global.css";
+
 export default function HomePage() {
-  return <main>{"Hello World  "} </main>;
+  return (
+    <>
+      <div className="text-greensharp">Hello Sir!!</div>
+      <div className="text-greendark">Hello Bhai</div>
+      <div className="text-greenprimary">Hello Dear</div>
+      <div className="text-greensecondary">Hello USA</div>
+      <div className="text-greentertiary">Hello Canada</div>
+      <div className="text-greenaccent">Hello India</div>
+      <div className="text-greenlight">Hello Pakistan</div>
+      <div className="text-graydark">Hello Pakistan</div>
+      <div className="text-graylight">Hello Pakistan</div>
+      <div className="h-screen w-screen layout-gradient"></div>
+    </>
+  );
 }
