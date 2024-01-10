6a262e1c8a0acf4f577db9d4d77c56e2462b4b7f
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-01-10 23:10:34 +0500

    feat: added new post dialog

--- src/Features/NewPost.tsx
+++ src/Features/NewPost.tsx
@@ -1,10 +1,22 @@
-import { Input } from "@/core/ui/input";
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 
+import { Input } from "@/core/ui/input";
+import NewPostDialog from "@/Features/Dialogs/NewPostDialog";
+
 const NewPost = () => {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleDialog = () => {
+    setOpenDialog(true);
+  };
   return (
     <>
-      <div className="border border-white-500 rounded-full w-full h-10 flex items-center p-3 mt-4 ">
+      <div
+        className="border border-white-500 rounded-full w-full h-10 flex items-center p-3 mt-4 "
+        onClick={handleDialog}
+      >
         <Input
           type="text"
           name="search_input"
@@ -19,6 +31,12 @@ const NewPost = () => {
           height={20}
         />
       </div>
+      {openDialog && (
+        <NewPostDialog
+          isOpen={openDialog}
+          onClose={() => setOpenDialog(false)}
+        />
+      )}
     </>
   );
 };
---
+++ src/core/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/core/lib/helper";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[120px] w-full rounded-2xl border border-input bg-greenaccent px-3 py-2 text-sm  placeholder:text-graydark focus-visible:outline-none border-none disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
