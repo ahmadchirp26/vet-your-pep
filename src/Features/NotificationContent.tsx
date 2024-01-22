136add4b8d47a7463899ae8ecfbaf32e34184a07
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-01-22 18:18:13 +0500

    fix: pr issues fixed

--- src/Features/NotificationContent.tsx
+++ src/Features/NotificationContent.tsx
@@ -8,7 +8,6 @@ const NotificationContent = () => {
       <div className="flex flex-col gap-2">
         <div className="mt-1">
           {notificationsArray.map((notification, index) => (
-            
             <NotificationCard key={index} notification={notification} />
           ))}
         </div>
