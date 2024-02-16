7d46b7bc8b647711621e74bd326cc5215eac509d
ahmadchirp26 <ahmad.sheraz@chirptech.net>
2024-02-16 18:09:36 +0500

    fix: lint issue

--- src/Features/NewChannels.tsx
+++ src/Features/NewChannels.tsx
@@ -1,7 +1,6 @@
 "use client";
 import ChannelCard from "@/Features/Cards/ChannelCard";
 import useGetChannels from "@/api/Channels/useGetChannels";
-import { SpinnerCircle } from "@/core/icons/SpinnerCircle";
 import { Skeleton } from "@/core/ui/skeleton";
 
 const NewChannels = () => {
@@ -10,20 +9,7 @@ const NewChannels = () => {
     joined: false,
   });
   const channelsArray = data?.getChannels.results;
-  if (status === "pending") {
-    //[Todo]: add a skeleton loader
-    return (
-      <div className="flex flex-col rounded-3xl container-drop-shadow bg-greendarkest p-4 min-w-[330px] max-xl:min-w-[160px] max-lg:min-w-[140px]">
-        <span className="text-white font-bold mt-3 max-lg:text-center">
-          New Channels
-        </span>
-
-        <span className="mt-5 text-greensharp text-center">
-          Error fetching Channels
-        </span>
-      </div>
-    );
-  }
+  
   return (
     <div className="flex flex-col rounded-3xl container-drop-shadow bg-greendarkest p-4 min-w-[330px] max-xl:min-w-[160px] max-lg:min-w-[140px]">
       <span className="text-white font-bold mt-3 max-lg:text-center">
