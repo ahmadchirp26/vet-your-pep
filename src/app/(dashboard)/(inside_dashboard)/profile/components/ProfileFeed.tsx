458cb96be577c0f4118872bf90a6e0e448f86389
ahmadchirp26 <ahmad.sheraz@chirptech.net>
2024-02-16 17:59:37 +0500

    fix: lint fixes

--- src/app/(dashboard)/(inside_dashboard)/profile/components/ProfileFeed.tsx
+++ src/app/(dashboard)/(inside_dashboard)/profile/components/ProfileFeed.tsx
@@ -1,16 +1,9 @@
-import NewPost from "@/Features/Post";
 import PostCard from "@/Features/Cards/PostCard";
 import { profilePosts } from "@/data/facebackend";
 const ProfileFeed = () => {
-  const postArray = profilePosts;
   return (
     <>
-      <div className=" w-full  p-4 max-sm:p-0 gap-3">
-        <NewPost />
-        {postArray.map((post, index) => (
-          <PostCard key={index} post={post} />
-        ))}
-      </div>
+      {}
     </>
   );
 };
