2c03098756f74b3038a58986870b998c73066e3d
ahmadchirp26 <ahmad.sheraz@chirptech.net>
2024-02-21 01:14:42 +0500

    refactor: convert to server component

--- src/app/(dashboard)/(inside_dashboard)/channels/[id]/edit/page.tsx
+++ src/app/(dashboard)/(inside_dashboard)/channels/[id]/edit/page.tsx
@@ -1,32 +1,18 @@
-"use client";
 import AllChannels from "@/Features/AllChannels";
 import ProfileCard from "@/Features/ProfileCard";
-import Banner from "../_components/Banner";
-import FeedPosts from "@/Features/Post/FeedPosts";
-import AboutCard from "../_components/AboutCard";
-import GroupAdmin from "../_components/GroupAdmin";
-import EventCard from "../_components/EventCard";
-import { Button } from "@/core/ui/button";
-import { useParams } from "next/navigation";
 import OnlineFriends from "@/Features/OnlineFriends";
-import EditChannel from "./components/EditChannelForm";
+import EditChannel from "./_components/EditChannelForm";
 
-const EditProfile = () => {
-  const { id } = useParams();
-  const channelId = id;
-  console.log("Channel ID", channelId);
-  // console.log(params);
+const EditProfile = (props: { params: { id: string } }) => {
   return (
     <>
       <div className="p-3 flex gap-5 max-md:flex-col">
         <div className="flex flex-col gap-4 min-w-[220px] max-md:hidden max-xl:min-w-[170px] max-lg:min-w-[150px]">
           <ProfileCard />
-
           <AllChannels />
         </div>
         <div className="flex flex-col gap-3 max-md:order-2 w-full">
-          {/* <Banner /> */}
-          <EditChannel channelId={channelId} />
+          <EditChannel channelId={props.params.id} />
         </div>
         <div className="max-md:order-1">
           <OnlineFriends className="flex md:flex-col" />
