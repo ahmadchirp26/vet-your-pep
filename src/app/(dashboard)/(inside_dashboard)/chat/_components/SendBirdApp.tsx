f6b1d2e85de02cbdfc698117a67e30a4aec4dc18
aitezazimtiaz <aitezaz.imtiaz@chirptech.net>
2024-02-27 00:01:09 +0500

    fix: fixed all event bugs

--- src/app/(dashboard)/(inside_dashboard)/chat/_components/SendBirdApp.tsx
+++ src/app/(dashboard)/(inside_dashboard)/chat/_components/SendBirdApp.tsx
@@ -1,29 +1,29 @@
-'use client';
-import React from "react";
-import SendbirdApp from "@sendbird/uikit-react/App";
-import './index.css'
-import { env } from "@/env";
+// 'use client';
+// import React from "react";
+// import SendbirdApp from "@sendbird/uikit-react/App";
+// import './index.css'
+// import { env } from "@/env";
 
-interface Props {
-  chatToken: string;
-  userId: string;
-  nickname: string;
-}
+// interface Props {
+//   chatToken: string;
+//   userId: string;
+//   nickname: string;
+// }
 
-const SendBirdApp = ({ chatToken,nickname,userId }: Props) => {
-  return (
-    <div className="w-full h-screen bg-greensharp">
-      <SendbirdApp
-        appId={env.NEXT_PUBLIC_SENDGRID_API_KEY}
-        userId={userId}
-        nickname={nickname}
-        accessToken={chatToken}
-        isMessageGroupingEnabled={false}
+// const SendBirdApp = ({ chatToken,nickname,userId }: Props) => {
+//   return (
+//     <div className="w-full h-screen bg-greensharp">
+//       <SendbirdApp
+//         appId={env.NEXT_PUBLIC_SENDGRID_API_KEY}
+//         userId={userId}
+//         nickname={nickname}
+//         accessToken={chatToken}
+//         isMessageGroupingEnabled={false}
 
-        theme="dark"
-      />
-    </div>
-  );
-};
+//         theme="dark"
+//       />
+//     </div>
+//   );
+// };
 
-export default SendBirdApp;
+// export default SendBirdApp;
